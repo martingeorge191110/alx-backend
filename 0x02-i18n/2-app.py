f4e61bc7ff018237c50a6b7d4543e012cadf0d42
Martin George <martingeorge191110@gmail.com>
2025-05-20 17:12:14 +0300

    Task 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """basic Flask app with single route"""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
@@ -17,10 +17,16 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale() -> str:
+    """returns the locale of a web page"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
 @app.route("/")
 def home() -> str:
-    """render 1-index.html page"""
-    return render_template('1-index.html')
+    """render 2-index.html page"""
+    return render_template('2-index.html')
 
 
 if __name__ == "__main__":
